6b92bd997aff92d5059df83e56b21ee6ea5e5f8c
Niclas <niclas.rogulski@gmail.com>
2026-04-21 22:39:42 +1000

    Improve theme tokens and overlays

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -5,7 +5,9 @@ import { AuthProvider } from './auth/AuthProvider';
 import { ToastProvider } from './components/toast/ToastProvider';
 import { router } from './routes/router';
 import { ThemeProvider } from './theme/ThemeProvider';
+import './styles/tokens.scss';
 import './styles/index.css';
+import './theme/theme.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <ThemeProvider>
